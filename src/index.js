e842d57390663681b3d90fcd141f33bf936b2f4f
Tshiamiso-Masilela <tshiamisomasilela14@gmail.com>
2025-07-25 16:20:13 +0200

    fixed some typos

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
-function formatDate(timestamp) {
+function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[date.getDay()];
@@ -62,14 +62,13 @@ function getForecast(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-function displayForecast(response) {}
-
-let forecastHtml = "";
-response.data.daily.forEach(function (day, index) {
-  if (index < 5) {
-    forecastHtml =
-      forecastHtml +
-      `
+function displayForecast(response) {
+  let forecastHtml = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
          <div class="weather-forecast-day">
         <div class="weather-forecast-date">${formatDay(day.time)}</div>
         <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
@@ -83,12 +82,12 @@ response.data.daily.forEach(function (day, index) {
           </div>
           </div>
         `;
-  }
-});
+    }
+  });
 
-let forecastElement = document.querySelector("#forecast");
-forecastElement.innerHTML = forecastHtml;
-
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
